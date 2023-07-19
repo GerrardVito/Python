0a60069619736a05bd886ca8847cb9ddd5df392a
GerrardVito <139851073+GerrardVito@users.noreply.github.com>
2023-07-18 19:51:08 -0700

    alternative that allowed minus and decimal

--- Integral.py
+++ Integral.py
@@ -1,4 +1,4 @@
-def integral(a: int, b:int= 1, x1=0, x2=0):
+def integral(a: int, b: int = 1, x1=0, x2=0):
     koef = a / (b + 1)
     pangkat = b + 1
     if x1 == 0 and x2 == 0:
@@ -6,14 +6,15 @@ def integral(a: int, b:int= 1, x1=0, x2=0):
     else:
         print(f"Hasil integrasi = {(koef * (int(x1) ** pangkat)) - (koef * (int(x2) ** pangkat))}")
 
+
 import re
+
 pattern = r"\b\d{1,3}(?:,\d{3})*(?:\.\d+)?\b"
-ygy=input("Integral dari (format: Koefisien x^ pangkat):")
+ygy = input("Integral dari (format: Koefisien x^ pangkat):")
 matches = re.findall(pattern, ygy)
 limita = input('Limit atas? : ')
 limitb = input('Limit bawah? : ')
 pros = []
 for bados in matches:
     pros.append(int(bados))
-integral(pros[0],pros[1] ,int(limita) ,int(limitb) )
-
+integral(pros[0], pros[1], int(limita), int(limitb))
--- text.py
+++ text.py
@@ -1,14 +1,25 @@
-y = 0
-g = ['x', 'x']
-ygy=input("Integral dari (format: Koefisien x^ pangkat):")
-limit = int(input("Limit 1: "))
-limit2=int(input ("Limit 2: "))
-for chai in ygy:
-    # noinspection PyBroadException
-    try:
-        d = int(chai)
-        g.append(d)
-    except:
-        pass
-print(g)
-integral(g[2], g[3], limit, limit2)
+import re
+
+def integral(a: int, b: int = 1, x1=0, x2=0):
+    koef = a / (b + 1)
+    pangkat = b + 1
+    if x1 == 0 and x2 == 0:
+        print(f"Hasil integrasi = {koef}x^{pangkat} + C")
+    else:
+        print(f"Hasil integrasi = {(koef * (int(x1) ** pangkat)) - (koef * (int(x2) ** pangkat))}")
+
+ygy = input("Integral dari (format: Koefisien x^ pangkat): ")
+
+# Extract Koefisien and pangkat using regular expressions
+pattern = r"(-?\d+(?:,\d+)*(?:\.\d+)?)x\^(-?\d+)"
+matches = re.findall(pattern, ygy)
+
+if matches:
+    koefisien, pangkat = matches[0]
+    integral(float(koefisien.replace(',', '')), int(pangkat))
+else:
+    print("Format input tidak valid.")
+
+limita = input("Limit atas: ")
+limitb = input("Limit bawah: ")
+integral(float(koefisien.replace(',', '')), int(pangkat), int(limita), int(limitb))
